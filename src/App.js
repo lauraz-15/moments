45d36085d5814e0f3f85fb1e500bee63ffb05869
lauraz-15 <laura.zvirbule90@gmail.com>
2022-08-02 15:21:22 +0000

    Add styles to main App.css and create styles folder

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import './App.css';
-import NavBar from './components/NavBar';
+import styles from "./App.module.css";
+import NavBar from "./components/NavBar";
+import Container from "react-bootstrap/Container";
 
 function App() {
   return (
-    <div className="App">
-      <NavBar/>
+    <div className={styles.App}>
+      <NavBar />
+      <Container className={styles.Main}>
+        <h1>Home page</h1>
+        <h1>Sing in</h1>
+      </Container>
     </div>
   );
 }
