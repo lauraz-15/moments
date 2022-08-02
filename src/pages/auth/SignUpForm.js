1c6b2fa17845e18bc5546d27b613c95ecae00724
lauraz-15 <laura.zvirbule90@gmail.com>
2022-08-02 20:15:31 +0000

    Create a sign up form

--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 import styles from "../../styles/SignInUpForm.module.css";
@@ -8,13 +8,63 @@ import appStyles from "../../App.module.css";
 import { Form, Button, Image, Col, Row, Container } from "react-bootstrap";
 
 const SignUpForm = () => {
+  const [signUpData, setSignUpData] = useState({
+    username: "",
+    password1: "",
+    password2: "",
+  });
+  const { username, password1, password2 } = signUpData;
+
+  const handleChange = (event) => {
+    setSignUpData({
+      ...signUpData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
   return (
     <Row className={styles.Row}>
       <Col className="my-auto py-2 p-md-2" md={6}>
         <Container className={`${appStyles.Content} p-4 `}>
           <h1 className={styles.Header}>sign up</h1>
 
-            {/* add your form here */}
+          <Form>
+              <Form.Group controlId="username">
+                <Form.Label className="d-none">username</Form.Label>
+                <Form.Control 
+                className={styles.Input}
+                type="email" 
+                placeholder="username"
+                name="username"
+                value="username"
+                onChange={handleChange}/>
+              </Form.Group>
+
+              <Form.Group controlId="password1">
+                <Form.Label className="d-none">Password</Form.Label>
+                <Form.Control 
+                className={styles.Input}
+                type="password" 
+                placeholder="Password"
+                name="password1"
+                value="password1"
+                onChange={handleChange} />
+              </Form.Group>
+
+              <Form.Group controlId="password2">
+                <Form.Label className="d-none">Confirm password</Form.Label>
+                <Form.Control 
+                className={styles.Input}
+                type="password" 
+                placeholder="Password"
+                name="password2"
+                value="password2" 
+                onChange={handleChange}/>
+              </Form.Group>
+              <Button className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`} type="submit">
+                Sign Up
+              </Button>
+          </Form>
 
         </Container>
         <Container className={`mt-3 ${appStyles.Content}`}>
