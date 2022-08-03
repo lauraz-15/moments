c0b0fe9f69a2963dfbfd5d3de975921666cc0dbb
lauraz-15 <laura.zvirbule90@gmail.com>
2022-08-03 11:06:45 +0000

    Add sign up errors and add post method

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import styles from "./App.module.css";
 import NavBar from "./components/NavBar";
 import Container from "react-bootstrap/Container";
-import {Route,Switch} from 'react-router-dom';
-import './api/axiosDefaults';
+import { Route, Switch } from "react-router-dom";
+import "./api/axiosDefaults";
 import SignUpForm from "./pages/auth/SignUpForm";
 
 function App() {
@@ -11,10 +11,10 @@ function App() {
       <NavBar />
       <Container className={styles.Main}>
         <Switch>
-          <Route exact path="/" render={() => <h1>Home page</h1>}/>
-          <Route exact path="/signin" render={() => <h1>Sing in</h1>}/>
-          <Route exact path="/signup" render={() => <SignUpForm/>}/>
-          <Route render={() => <p>Page not found</p>}/>
+          <Route exact path="/" render={() => <h1>Home page</h1>} />
+          <Route exact path="/signin" render={() => <h1>Sign in</h1>} />
+          <Route exact path="/signup" render={() => <SignUpForm />} />
+          <Route render={() => <p>Page not found!</p>} />
         </Switch>
       </Container>
     </div>
